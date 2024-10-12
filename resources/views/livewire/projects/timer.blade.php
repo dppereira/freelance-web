eb767638204def8bba80ce3f56e1da64819fe51b
dppereira <diogoppereira504@gmail.com>
2024-10-11 23:40:14 -0300

    Timer

---
+++ resources/views/livewire/projects/timer.blade.php
@@ -0,0 +1,9 @@
+<div wire:poll.1s class="flex items-center justify-between mt-4 text-[14px]">
+    <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
+    <div class="font-bold flex items-center space-x-1">
+        <span class="text-white ">{{ $days }}</span><span>:</span>
+        <span class="text-white ">{{ $hours }}</span><span>:</span>
+        <span class="text-white ">{{ $min }}</span><span>:</span>
+        <span class="text-white ">{{ $sec }}</span>
+    </div>
+</div>
